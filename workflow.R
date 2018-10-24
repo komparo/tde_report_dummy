4cd90630b91d379c83344211085171a8e1eda758
zouter <wouter.saelens@gmail.com>
2018-10-24 17:03:16 +0200

    generate report in tidyverse docker where rmarkdown is available

--- workflow.R
+++ workflow.R
@@ -10,11 +10,15 @@ generate_report_calls <- function(datasets, methods, metrics, workflow_folder = 
     "komparo/dummy",
     inputs = list(
       script = script_file(str_glue("{workflow_folder}/scripts/run.R")),
-      markdown = script_file(str_glue("{workflow_folder}/scripts/report.Rmd"))
+      markdown = script_file(str_glue("{workflow_folder}/scripts/report.Rmd")),
+      executor = docker_executor(container = "rocker/tidyverse")
     ) %>% c(
       datasets$outputs %>% select(meta, expression) %>% map(object_set),
       methods$outputs %>% select(tde_overall) %>% map(object_set),
-      metrics$outputs %>% select(accuracy) %>% map(object_set)
+      bind_cols(metrics$outputs, metrics$design) %>% 
+        select(dataset_id, method_id, accuracy) %>% 
+        mutate(dataset_id = map(dataset_id, parameters), method_id = map(method_id, parameters)) %>% 
+        map(object_set)
     ),
     outputs = str_glue("{reports_folder}/index.html") %>% map(derived_file) %>% set_names("report")
   )
