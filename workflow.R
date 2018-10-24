f9d51a76a30e98021d24a99a332ffc6f696e7bc8
zouter <wouter.saelens@gmail.com>
2018-10-24 21:39:55 +0200

    update and simplify for new certigo

--- workflow.R
+++ workflow.R
@@ -6,11 +6,10 @@ library(stringr)
 library(dplyr)
 
 generate_report_calls <- function(datasets, methods, metrics, workflow_folder = ".", reports_folder = "report") {
-  rscript_call(
+  rmd_call(
     "komparo/dummy",
     inputs = list(
-      script = script_file(str_glue("{workflow_folder}/scripts/run.R")),
-      markdown = script_file(str_glue("{workflow_folder}/scripts/report.Rmd")),
+      script = script_file(str_glue("{workflow_folder}/scripts/report.Rmd")),
       executor = docker_executor(container = "rocker/tidyverse")
     ) %>% c(
       datasets$outputs %>% select(meta, expression) %>% map(object_set),
@@ -20,6 +19,6 @@ generate_report_calls <- function(datasets, methods, metrics, workflow_folder = 
         mutate(dataset_id = map(dataset_id, parameters), method_id = map(method_id, parameters)) %>% 
         map(object_set)
     ),
-    outputs = str_glue("{reports_folder}/index.html") %>% map(derived_file) %>% set_names("report")
+    outputs = str_glue("{reports_folder}/index.html") %>% map(derived_file) %>% set_names("rendered")
   )
 }
