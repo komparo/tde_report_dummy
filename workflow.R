e423dbd6e31c70584204f76c0d761c0354ea1250
Wouter Saelens <wouter.saelens@gmail.com>
2018-10-25 17:15:07 +0200

    update with new certigo

--- workflow.R
+++ workflow.R
@@ -6,19 +6,18 @@ library(stringr)
 library(dplyr)
 
 generate_report_calls <- function(datasets, methods, metrics, workflow_folder = ".", reports_folder = "report") {
+  design <- list(
+    script = script_file(str_glue("{workflow_folder}/scripts/report.Rmd")),
+    executor = docker_executor(container = "rocker/tidyverse"),
+    rendered = derived_file(str_glue("{reports_folder}/index.html")),
+    datasets = datasets$design %>% select(meta, expression) %>% map(object_set) %>% object_set(),
+    scores = metrics$design %>% select(dataset_id, method_id, accuracy) %>% map(object_set) %>% object_set()
+  )
+  
   rmd_call(
     "komparo/dummy",
-    inputs = list(
-      script = script_file(str_glue("{workflow_folder}/scripts/report.Rmd")),
-      executor = docker_executor(container = "rocker/tidyverse")
-    ) %>% c(
-      datasets$outputs %>% select(meta, expression) %>% map(object_set),
-      methods$outputs %>% select(tde_overall) %>% map(object_set),
-      bind_cols(metrics$outputs, metrics$design) %>% 
-        select(dataset_id, method_id, accuracy) %>% 
-        mutate(dataset_id = map(dataset_id, parameters), method_id = map(method_id, parameters)) %>% 
-        map(object_set)
-    ),
-    outputs = str_glue("{reports_folder}/index.html") %>% map(derived_file) %>% set_names("rendered")
+    design = design,
+    inputs = c("script", "executor", "datasets", "scores"),
+    outputs = "rendered"
   )
 }
