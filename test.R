d52da5cfa62a19dabe5f6d12c6a23b71801ba653
Wouter Saelens <wouter.saelens@gmail.com>
2018-10-26 05:49:23 +0200

    would this fix travis?

--- test.R
+++ test.R
@@ -15,7 +15,7 @@ datasets <- generate_dataset_calls(
   workflow_folder = "modules/dataset",
   datasets_folder = "data/datasets",
   dataset_design = dataset_design_all[1:4, ]
-) %>% call_collection(id = "datasets")
+)
 
 source("modules/method/workflow.R")
 methods <- generate_method_calls(
@@ -23,7 +23,7 @@ methods <- generate_method_calls(
   method_design = method_design_all[1, ],
   datasets = datasets,
   models_folder = "data/models"
-) %>% call_collection(id = "methods")
+)
 
 source("modules/metric/workflow.R")
 metrics <- generate_metric_calls(
@@ -31,14 +31,14 @@ metrics <- generate_metric_calls(
   metric_design = metric_design_all[1, ],
   methods = methods,
   scores_folder = "data/scores"
-) %>% call_collection(id = "metrics")
+)
 
 source('workflow.R')
 report <- generate_report_calls(
   datasets = datasets,
   methods = methods,
   metrics = metrics
-) %>% call_collection(id = "reports")
+)
 
 workflow <- workflow(
   datasets,
